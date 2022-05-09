fe6c8e25bd9c32a796f4f0d792b2dfc9ec3c6e89
shanacaroti <shana.caroti@students.fhnw.ch>
2022-05-09 10:21:50 +0200

    svg

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import eyeSvg from './resources/RoundEye.svg';
+import SvgComponent from './components/svgComponent';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+        <div>
+            <SvgComponent/>
+            <SvgComponent/>
+        </div>
     </div>
   );
 }
---
+++ src/components/svgComponent.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const SvgComponent = () => {
+    return (
+        <svg id="{id}" viewBox="0 0 120 120">
+
+            <filter id="shadow">
+                <feGaussianBlur in="SourceAlpha" stdDeviation="3" />
+                <feOffset       dx="0" dy="8" />
+                <feColorMatrix  type="matrix"
+                                values="0 0 0 0  0
+                                0 0 0 0  0
+                                0 0 0 0  0
+                                0 0 0 .5 0"/>
+                <feBlend        in="SourceGraphic" mode="normal"/>
+            </filter>
+
+            <radialGradient id="gradient1" gradientUnits="objectBoundingBox" cx="50%" cy="50%" r="50%">
+                <stop offset= "38%" stopColor="#000000" stopOpacity="1" />
+                <stop offset= "46%" stopColor="#073F80" stopOpacity="1" />
+                <stop offset= "90%" stopColor="#8EC0DD" stopOpacity="1" />
+                <stop offset="100%" stopColor="#2F3A46" stopOpacity="1" />
+            </radialGradient>
+            <g id="{id}_iris">
+                <ellipse cx="60" cy="60" rx="30" ry="30" opacity="1" fill="url(#gradient1)" />
+                <ellipse cx="50" cy="50" rx="7"  ry="7"  opacity="1" fill="#FFFFFF" fillOpacity="0.8"/>
+            </g>
+            <g id="{id}_openLids">
+                <path d="M0 60 A60,60 0 0,1 120,60 A60,30 0 0,0 0,60 Z" opacity="1" fill="#FDDC99" fillOpacity="1" filter="url(#shadow)" />
+                <path d="M0 60 A60,60 0 0,0 120,60 A60,40 0 0,1 0,60 Z" opacity="1" fill="#F4CB76" fillOpacity="1" />
+            </g>
+            <g id="{id}_closeLid">
+                <path d="M0 60 A60,60 0 0,1 120,60 A60,40 0 0,1 0,60 Z" opacity="1" fill="#FDDC99" fillOpacity="1" />
+            </g>
+        </svg>
+    );
+}
+
+export default SvgComponent;

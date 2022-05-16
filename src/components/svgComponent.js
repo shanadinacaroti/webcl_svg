8d89dd7e24a4dffab24ccb5faf46323574b80f57
shanacaroti <shana.caroti@students.fhnw.ch>
2022-05-16 15:02:49 +0200

    add eye-following mouse movement without any rules

--- src/components/svgComponent.js
+++ src/components/svgComponent.js
@@ -1,7 +1,8 @@
-import { useState } from 'react';
+import {useEffect, useState} from 'react';
 
 export function SvgComponent() {
     const [opac, setOpac] = useState(0);
+    const [globalCoords, setGlobalCoords] = useState({x: 0, y: 0});
 
     function openEyes() {
         setOpac(0);
@@ -12,12 +13,26 @@ export function SvgComponent() {
     }
 
     function eyeMovement() {
-        if (opac === 0) {
-            closeEyes();
-        } else {
-            openEyes();
-        }
+        opac === 0 ? closeEyes() : openEyes();
+
     }
+
+    useEffect(() => {
+        const handleWindowMouseMove = event => {
+            console.log("x", event.screenX);
+            console.log("y", event.screenY);
+            setGlobalCoords({
+                x: event.screenX - 450,
+                y: event.screenY - 350,
+            });
+
+        };
+        window.addEventListener('mousemove', handleWindowMouseMove);
+
+        return () => {
+            window.removeEventListener('mousemove', handleWindowMouseMove);
+        };
+    }, []);
 
     return (
         <svg id="{id}" viewBox="0 0 120 120" onClick={eyeMovement}>
@@ -40,7 +55,7 @@ export function SvgComponent() {
                 <stop offset="100%" stopColor="#2F3A46" stopOpacity="1" />
             </radialGradient>
             <g id="{id}_iris">
-                <ellipse cx="60" cy="60" rx="30" ry="30" opacity="1" fill="url(#gradient1)" />
+                <ellipse cx={globalCoords.x} cy={globalCoords.y} rx="30" ry="30" opacity="1" fill="url(#gradient1)" />
                 <ellipse cx="50" cy="50" rx="7"  ry="7"  opacity="1" fill="#FFFFFF" fillOpacity="0.8"/>
             </g>
             <g id="{id}_openLids">
